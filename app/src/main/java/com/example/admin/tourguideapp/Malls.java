7471cf2c074777814ae4a67f6f8a324f2f6b980b
BhaveshBabel <bhaveshbabel86@gmail.com>
2018-08-30 00:06:14 +0530

    Created Malls activity.

--- app/src/main/java/com/example/admin/tourguideapp/Malls.java
+++ app/src/main/java/com/example/admin/tourguideapp/Malls.java
@@ -2,12 +2,27 @@ package com.example.admin.tourguideapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Malls extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_malls);
+        String mallName1 = getResources().getString(R.string.mallName1);
+        String mallName2 = getResources().getString(R.string.mallName2);
+        String mallAdd1 = getResources().getString(R.string.mallAdd1);
+        String mallAdd2 = getResources().getString(R.string.mallAdd2);
+
+        ArrayList<Location> words = new ArrayList<Location>();
+
+        words.add(new Location(mallName1, mallAdd1));
+        words.add(new Location(mallName2, mallAdd2));
+
+        LocationAdapter adapter = new LocationAdapter(this, words, R.color.malls);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }

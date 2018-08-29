05df9a229ce3f7d850be076dea5abb0dce4113bc
BhaveshBabel <bhaveshbabel86@gmail.com>
2018-08-29 18:36:31 +0530

    Added Attractions activity.

--- app/src/main/java/com/example/admin/tourguideapp/Attractions.java
+++ app/src/main/java/com/example/admin/tourguideapp/Attractions.java
@@ -2,12 +2,26 @@ package com.example.admin.tourguideapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Attractions extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attractions);
+        String attrName1 = getResources().getString(R.string.attrName1);
+        String attrName2 = getResources().getString(R.string.attrName2);
+        String attrAdd1 = getResources().getString(R.string.attrAdd1);
+        String attrAdd2 = getResources().getString(R.string.attrAdd2);
+
+        ArrayList<Location> words = new ArrayList<Location>();
+
+        words.add(new Location(attrName1, attrAdd1));
+        words.add(new Location(attrName2, attrAdd2));
+        LocationAdapter adapter = new LocationAdapter(this, words, R.color.attractions);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }
